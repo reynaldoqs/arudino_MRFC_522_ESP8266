133a6c295681dcf5886dd308c28de7b08f6e4d1a
Reynaldo Quispe Sanchez <reynaldoqs@gmail.com>
2019-11-24 10:31:08 -0400

    updateing to node js routes

--- sketch_test_esp_card_reader.ino
+++ sketch_test_esp_card_reader.ino
@@ -1,14 +1,12 @@
 #include <SPI.h>
 #include <MFRC522.h>
 #include <ESP8266WiFi.h>
-#include <FirebaseArduino.h>
+#include <ESP8266HTTPClient.h>
+#include <ArduinoJson.h>
 
-// Configuracion firebase
-#define FIREBASE_HOST ""
-#define FIREBASE_AUTH ""
 // red y contrasenia wifi
-#define WIFI_SSID ""
-#define WIFI_PASSWORD ""
+#define WIFI_SSID "AMD2"
+#define WIFI_PASSWORD "1989REY"
 
 //pins MRF22
 #define SS_PIN D4
@@ -30,80 +28,60 @@ void setup() {
   Serial.print("connected: ");
   Serial.println(WiFi.localIP());
   
-  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
-  
   //card reader start
   SPI.begin();
   mfrc522.PCD_Init(); 
   Serial.println("Control de acceso:");
+  
 }
 
- String firebasePath = "/users/";
- int passCost = 20;
+ String service_url = "http://192.168.0.2:3000/socket_desc"; //ip host, it would change on every instance
+
 
 void loop() {
-  if ( mfrc522.PICC_IsNewCardPresent()){
-      if ( mfrc522.PICC_ReadCardSerial()){
-        Serial.println(""); 
-        Serial.print("tag UID");
-        String cardId = "";
-        for (byte i = 0; i < mfrc522.uid.size; i++) {
-               Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-               Serial.print(mfrc522.uid.uidByte[i], HEX);  
-               cardId += mfrc522.uid.uidByte[i];
-               //para parsear a hexa el id
-               //cardId.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? "_0" : "_"));
-               //cardId.concat(String(mfrc522.uid.uidByte[i], HEX));
-            }
-                  //cardId.toUpperCase();
-                  Serial.println(""); 
-                  Serial.print("firebase id: ");
-                  Serial.println(cardId);
-                  String path = firebasePath+cardId;
 
-                  String userName = "";
-                  String userNameReaded = Firebase.getString(path+"/nombre");
-                  if(Firebase.success()){
-                    userName = userNameReaded;
-                    }
-
-                  int userBid = -1;
-                  int userBidReaded = Firebase.getInt(path+"/credito");
-                  if(Firebase.success()){
-                    userBid = userBidReaded;
-                   }
+    if ( mfrc522.PICC_IsNewCardPresent()){
+        if ( mfrc522.PICC_ReadCardSerial()){
+          Serial.println("-------------------------------------------------"); 
+          String cardId = "";
+          for (byte i = 0; i < mfrc522.uid.size; i++) {
+                 Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+                 Serial.print(mfrc522.uid.uidByte[i], HEX);  
+                 cardId += mfrc522.uid.uidByte[i];
+              }
+                    Serial.println(""); 
+                    Serial.print("idTarjeta: ");
+                    Serial.println(cardId);
+                    
+                    StaticJsonDocument<100> testDocument;
+                    testDocument["terminalMac"] = "fr:fr:54:r4";
+                    testDocument["idTarjeta"] = cardId;
+                    
+                    char buffer[100];
+                    serializeJson(testDocument, buffer);
                   
-                   Serial.println(userName);
-                  //mejorar esta validacion
-                  if(userName != "" && userBid != -1){          
-                      
-                    if((userBid - passCost) < 0){
-           
-                            Serial.print("Credito insuficiente: ");
-                            Serial.print(userBid);
-                            Serial.println(" Bs");
-                            return;
-                          }else{
-                          Serial.println("success firebase get");
-                          
-                          Firebase.setInt(path+"/credito", (userBid-passCost));// descontando el costo del pase 
-                          if(Firebase.failed()){
-                            Serial.println("Error al descontar el monto");
-                            return;
-                           }
-                          
-                          Serial.print("Credito restante: ");
-                          Serial.print(userBid-passCost);//actualizando solo para mostrar
-                          Serial.println(" Bs");    
-                          }
-               
-                    }else{
-                      Serial.println("Tarjeta no registrada o error en la conexion");
-                      }
-                  
-                  mfrc522.PICC_HaltA(); 
-                  
-        }
-    }
+                    HTTPClient http; 
+                    http.begin(service_url); 
+                    http.addHeader("Content-Type", "application/json");  //Specify content-type header
+   
+                    int httpCode = http.POST(buffer);
+                    String payload = http.getString();   
+  
+                    if(httpCode >= 200 && httpCode < 300){
+                        Serial.println("Everithing is ok");
+                        Serial.println(payload);
+                      }else{
+                        Serial.println("Something has been fallen");
+                        Serial.println(payload);
+                        }
+                     
+                    
+                    http.end();
+                    
+                    mfrc522.PICC_HaltA(); 
+                    
+          }
+      }
+ 
 
 }
